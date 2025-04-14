4efac3f0a0804a5b33099e258052e01128851979
oughhhhh <oughhhhh@proton.me>
2025-04-14 22:52:05 +0300

    inference

---
+++ predict.py
@@ -0,0 +1,15 @@
+from PIL.Image import Image
+from ultralytics import YOLO
+
+
+def get_boxes_xywh(image : Image) -> list:
+    res = []
+
+    model = YOLO("best.pt")  # pretrained YOLO11n model
+    results = model(image)
+
+    for result in results:
+        for box in result.boxes:
+            res.append(box.xywh[0].to("cpu"))
+
+    return res
