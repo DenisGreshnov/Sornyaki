8906ed19992fa578cc825c10e22defbbccd09c32
oughhhhh <oughhhhh@proton.me>
2025-05-17 20:30:46 +0300

    front midway

--- app.py
+++ app.py
@@ -8,43 +8,46 @@ from utils import process_image
 
 app = Flask(__name__)
 app.config['UPLOAD_FOLDER'] = 'static'
+app.secret_key = 'BAD_SECRET_KEY'
+
+context = {}
+context["images"] = []
+
 
 def check_allowed_file(filename): return True
 def secure_filename(filename): return filename
 
 
-@app.route("/",methods=['GET', 'POST'])
-def index():
-    context = {}
-    context["images"] = []
+@app.route("/",methods=['POST'])
+def index_post():
 
-    if request.method == 'POST':
-        print('post', request.files)
-        if 'file' not in request.files:
-            print('No file attached in request')
-            return redirect(request.url)
+    print('post', request.files)
+    if 'file' not in request.files:
+        print('No file attached in request')
+        return redirect(request.url)
 
-        file = request.files['file']
-        if file.filename == '':
-            print('No file selected')
-            return redirect(request.url)
+    file = request.files['file']
+    if file.filename == '':
+        print('No file selected')
+        return redirect(request.url)
 
-        if not (file and check_allowed_file(file.filename)):
-            return redirect(request.url)
+    if not (file and check_allowed_file(file.filename)):
+        return redirect(request.url)
 
-        filename = secure_filename(file.filename)
-        img = PIL.Image.open(file.stream)
+    filename = secure_filename(file.filename)
+    img = PIL.Image.open(file.stream)
 
-        os.makedirs("static/temp", exist_ok=True)
-        processed_image = process_image(img).image
-        processed_image.save(f"static/temp/{filename}")
+    os.makedirs("static/temp", exist_ok=True)
+    processed_image = process_image(img).image
+    processed_image.save(f"static/temp/{filename}")
 
-        context["images"].append({"path" : f"temp/{filename}"})
-        return render_template('index.html', context=context), 200
+    context["images"].append({"path" : f"temp/{filename}"})
+    return render_template('display.html', context=context), 200
 
-    else:
-        context["images"].append({"path" : "image1.jpg"})
-        return render_template("index.html", context=context), 200
+
+@app.route("/",methods=['GET'])
+def index_get():
+    return render_template("base.html"), 200
 
 
 if __name__ == '__main__':
