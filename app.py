594b32af9040945bfab0f04d5b17ec154b4b3137
oughhhhh <oughhhhh@proton.me>
2025-05-17 22:06:25 +0300

    del kinda works

--- app.py
+++ app.py
@@ -1,9 +1,9 @@
 import os
 import PIL.Image
 from flask import Flask, request, redirect
-from flask import render_template, session
+from flask import render_template, session, redirect, url_for
 from flask_session import Session
-
+from uuid import uuid4
 from utils import process_image
 
 
@@ -18,11 +18,33 @@ def check_allowed_file(filename): return True
 def secure_filename(filename): return filename
 
 
-@app.route("/",methods=['POST'])
-def index_post():
-    context = {}
+@app.route("/delete/<ind>", methods=["POST", "GET"])
+def delete_image(ind):
+    if not session.get("images"):
+        print("No images to delete")
+        return redirect(url_for('index'), 301)
 
-    print('post', request.files)
+    try:
+        ind = int(ind)
+        session["images"].pop(ind)
+    except ValueError as e:
+        print(f"Error deleting {ind}, {e}")
+
+    return redirect(url_for('index'), 301)
+
+
+@app.route("/",methods=['POST', 'GET'])
+def index():
+    if not session.get("images"): session["images"] = []
+    context = {}
+    context["images"] = session["images"]
+
+    if request.method == 'GET':
+        return render_template('display.html', context=context, enumerate=enumerate), 200
+
+    print('post', request.content_type, request.files)
+
+
     if 'file' not in request.files:
         print('No file attached in request')
         return redirect(request.url)
@@ -36,25 +58,19 @@ def index_post():
         return redirect(request.url)
 
     filename = secure_filename(file.filename)
-    img = PIL.Image.open(file.stream)
+    try:
+        img = PIL.Image.open(file.stream)
+    except IOError:
+        return redirect(request.url)
 
     os.makedirs("static/temp", exist_ok=True)
     processed_image = process_image(img).image
     processed_image.save(f"static/temp/{filename}")
 
-    if not session.get("images"): session["images"] = []
     session["images"].append({"path" : f"temp/{filename}"})
     context["images"] = session["images"]
-    return render_template('display.html', context=context), 200
-
-
-@app.route("/",methods=['GET'])
-def index_get():
-    context = {}
-    if not session.get("images"): session["images"] = []
-    context["images"] = session["images"]
-    return render_template('display.html', context=context), 200
+    return render_template('display.html', context=context, enumerate=enumerate), 200
 
 
 if __name__ == '__main__':
-    app.run()
+    app.run(debug=True)
