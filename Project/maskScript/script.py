b443d51f2994294ac4d7bb19d489a8e02902f78d
Shegem <121248687+ShegemArt@users.noreply.github.com>
2025-05-23 17:06:37 +0300

    Add deleting class "tobacco" from mask

--- Project/maskScript/script.py
+++ Project/maskScript/script.py
@@ -1,18 +1,33 @@
 import cv2
 import numpy as np
+from ultralytics import YOLO
 
-image = cv2.imread("Test_Image.jpg")  # замените на путь к вашей фотографии
+model = YOLO("yolovT.pt")  # Веса
+
+image = cv2.imread("Test_Image.jpg")  # Свой путь
 image_rgb = cv2.cvtColor(image, cv2.COLOR_BGR2RGB)
+
+results = model(image_rgb)[0]
+
+mask_tobacco = np.zeros(image.shape[:2], dtype=np.uint8)
+
+for box, cls in zip(results.boxes.xyxy, results.boxes.cls):
+    class_name = model.names[int(cls)]
+    # Для проверки какой класс
+    # print(f"Detected: {class_name}")
+
+    if "tobacco" in class_name.lower():
+        x1, y1, x2, y2 = map(int, box)
+        cv2.rectangle(mask_tobacco, (x1, y1), (x2, y2), 255, -1)
 
 hsv = cv2.cvtColor(image, cv2.COLOR_BGR2HSV)
 
-# Диапазон для зелёного
 lower_green = np.array([35, 40, 40])
 upper_green = np.array([85, 255, 255])
 
-# Бинарная маска зелёного цвета
 green_mask = cv2.inRange(hsv, lower_green, upper_green)
 
-# Визуализация
+green_mask[mask_tobacco == 255] = 0
+
 cv2.imshow("Зелёная маска без табака", green_mask)
 cv2.waitKey(0)
