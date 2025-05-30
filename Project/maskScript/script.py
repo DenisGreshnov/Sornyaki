3495edecf884219a900be84c5eefcb4002cac5c1
Shegem <121248687+ShegemArt@users.noreply.github.com>
2025-05-30 20:40:49 +0300

    Add using parameters of tobacco boxes to delete noise

--- Project/maskScript/script.py
+++ Project/maskScript/script.py
@@ -4,11 +4,14 @@ from ultralytics import YOLO
 
 model = YOLO("model.pt")
 
-image = cv2.imread("Test_Image.jpg")
+image = cv2.imread("71.jpg")
 
 results = model(image)[0]
 
 mask_tobacco = np.zeros(image.shape[:2], dtype=np.uint8)
+
+mean_squre = 0
+count = 0
 
 for box, cls in zip(results.boxes.xyxy, results.boxes.cls):
     class_name = model.names[int(cls)]
@@ -16,6 +19,13 @@ for box, cls in zip(results.boxes.xyxy, results.boxes.cls):
     if "tobacco" in class_name.lower():
         x1, y1, x2, y2 = map(int, box)
         cv2.rectangle(mask_tobacco, (x1, y1), (x2, y2), 255, -1)
+        mean_squre += 2 * (abs(x1 - x2) + abs(y1 - y2))
+        count+=1
+
+print(mean_squre)
+mean_squre /= count
+mean_squre = int(mean_squre)
+print(mean_squre)
 
 hsv = cv2.cvtColor(image, cv2.COLOR_BGR2HSV)
 lower_green = np.array([35, 40, 40])
@@ -28,7 +38,7 @@ contours, _ = cv2.findContours(green_mask, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_S
 weed_clusters = []
 for cnt in contours:
     area = cv2.contourArea(cnt)
-    if area > 50: #Настраиваемый параметр
+    if area > 50 and area < mean_squre: #Настраиваемый параметр
         M = cv2.moments(cnt)
         if M["m00"] != 0:
             cx = int(M["m10"] / M["m00"])
@@ -39,7 +49,8 @@ for cnt in contours:
 #Координаты центров скоплений сорняков"
 print(weed_clusters)
 
-cv2.imshow("Зелёная маска без табака", green_mask)
+
+# cv2.imshow("Зелёная маска без табака", green_mask)
 cv2.imshow("Центры сорняков", image)
 
 cv2.waitKey(0)
