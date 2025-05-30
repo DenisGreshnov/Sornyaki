32fcdc44cab604950dda969b531667283144a1ce
Shegem <121248687+ShegemArt@users.noreply.github.com>
2025-05-30 20:02:27 +0300

    Add getting middle points of weed and its visualization

--- Project/maskScript/script.py
+++ Project/maskScript/script.py
@@ -2,32 +2,45 @@ import cv2
 import numpy as np
 from ultralytics import YOLO
 
-model = YOLO("yolovT.pt")  # Веса
+model = YOLO("model.pt")
 
-image = cv2.imread("Test_Image.jpg")  # Свой путь
-image_rgb = cv2.cvtColor(image, cv2.COLOR_BGR2RGB)
+image = cv2.imread("Test_Image.jpg")
 
-results = model(image_rgb)[0]
+results = model(image)[0]
 
 mask_tobacco = np.zeros(image.shape[:2], dtype=np.uint8)
 
 for box, cls in zip(results.boxes.xyxy, results.boxes.cls):
     class_name = model.names[int(cls)]
-    # Для проверки какой класс
-    # print(f"Detected: {class_name}")
 
     if "tobacco" in class_name.lower():
         x1, y1, x2, y2 = map(int, box)
         cv2.rectangle(mask_tobacco, (x1, y1), (x2, y2), 255, -1)
 
 hsv = cv2.cvtColor(image, cv2.COLOR_BGR2HSV)
-
 lower_green = np.array([35, 40, 40])
 upper_green = np.array([85, 255, 255])
-
 green_mask = cv2.inRange(hsv, lower_green, upper_green)
-
 green_mask[mask_tobacco == 255] = 0
 
+contours, _ = cv2.findContours(green_mask, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
+
+weed_clusters = []
+for cnt in contours:
+    area = cv2.contourArea(cnt)
+    if area > 50: #Настраиваемый параметр
+        M = cv2.moments(cnt)
+        if M["m00"] != 0:
+            cx = int(M["m10"] / M["m00"])
+            cy = int(M["m01"] / M["m00"])
+            weed_clusters.append((cx, cy))
+            cv2.circle(image, (cx, cy), 5, (0, 0, 255), -1) #Рисует точки красным на изначальном изображении
+
+#Координаты центров скоплений сорняков"
+print(weed_clusters)
+
 cv2.imshow("Зелёная маска без табака", green_mask)
+cv2.imshow("Центры сорняков", image)
+
 cv2.waitKey(0)
+cv2.destroyAllWindows()
